ca2ebe5c8e03a97279ada3aa6111fea831742b6b
ckckckcz <rriovld@gmail.com>
2024-10-14 10:38:25 +0700

    update: change model navbar

--- Client/src/views/Home.tsx
+++ Client/src/views/Home.tsx
@@ -1,12 +1,8 @@
 import Navbar from "../components/Navbar";
-import Section1 from "../components/Home/Section1";
-import Section2 from "../components/Home/Section2";
 export default function Home() {
   return (
     <>
-    <Navbar />
-    <Section1/>
-    <Section2/>
+      <Navbar />
     </>
   );
 }
