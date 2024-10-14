4b33ba81116b322f786a2e50bc518fa142e0cd9c
ckckckcz <rriovld@gmail.com>
2024-10-14 13:20:45 +0700

    update: card at tips

--- Client/src/components/Home/Tips.tsx
+++ Client/src/components/Home/Tips.tsx
@@ -1,13 +1,26 @@
 export default function Tips() {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 flex items-center justify-center" style={{ marginTop: "517px" }}>
-      <div className="py-8 px-4 text-center mx-auto max-w-screen-xl">
+    <section className="bg-gray-50 dark:bg-gray-900 flex items-center justify-center sectionTips py-8 px-4">
+      <div className="text-center mx-auto max-w-screen-xl">
+        <style>{`
+          .sectionTips{
+            margin-top: 517px;
+          }
+          @media(max-width: 767px){
+            .sectionTips{
+              margin-top:180px;
+            }
+          }
+        `}</style>
+        <h1 className="mt-10 mb-12 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          3 Cara untuk <span className="text-red-600 dark:text-red-500">Memilih Pengacara</span>
+        </h1>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-7 text-left">
           <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Riset Online</h5>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Cari pengacara melalui situs hukum terpercaya atau ulasan online. Bandingkan pengalaman, spesialisasi, dan biaya layanan mereka.</p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Cari pengacara melalui berbagai situs hukum terpercaya atau platform ulasan online yang menyediakan informasi mengenai pembahasan hukum.</p>
             <a
               href="#"
               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
