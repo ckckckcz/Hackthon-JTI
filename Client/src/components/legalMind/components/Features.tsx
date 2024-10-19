8cfdf12ecd6c37e13294380d8d802d04b7055731
ckckckcz <rriovld@gmail.com>
2024-10-19 14:11:00 +0700

    update: change color background at features

--- Client/src/components/legalMind/components/Features.tsx
+++ Client/src/components/legalMind/components/Features.tsx
@@ -12,7 +12,7 @@ export default function Features() {
             className="inline-flex lg:w-[290px] w-[320px] items-center justify-between px-5 py-3 text-base font-medium text-center text-black border border-gray-200 bg-white rounded-xl hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mx-auto"
           >
             <div className="title flex gap-3 items-center font-bold">
-              <svg className="w-10 h-10 text-blue-800 rounded-xl bg-blue-600 p-2 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <svg className="w-10 h-10 text-blue-500 rounded-xl bg-blue-100 p-2 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path
                   fillRule="evenodd"
                   d="M12 4a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm-2.952.462c-.483.19-.868.432-1.19.71-.363.315-.638.677-.831.93l-.106.14c-.21.268-.36.418-.574.527C6.125 6.883 5.74 7 5 7a1 1 0 0 0 0 2c.364 0 .696-.022 1-.067v.41l-1.864 4.2a1.774 1.774 0 0 0 .821 2.255c.255.133.538.202.825.202h2.436a1.786 1.786 0 0 0 1.768-1.558 1.774 1.774 0 0 0-.122-.899L8 9.343V8.028c.2-.188.36-.38.495-.553.062-.079.118-.15.168-.217.185-.24.311-.406.503-.571a1.89 1.89 0 0 1 .24-.177A3.01 3.01 0 0 0 11 7.829V20H5.5a1 1 0 1 0 0 2h13a1 1 0 1 0 0-2H13V7.83a3.01 3.01 0 0 0 1.63-1.387c.206.091.373.19.514.29.31.219.532.465.811.78l.025.027.02.023v1.78l-1.864 4.2a1.774 1.774 0 0 0 .821 2.255c.255.133.538.202.825.202h2.436a1.785 1.785 0 0 0 1.768-1.558 1.773 1.773 0 0 0-.122-.899L18 9.343v-.452c.302.072.633.109 1 .109a1 1 0 1 0 0-2c-.48 0-.731-.098-.899-.2-.2-.12-.363-.293-.651-.617l-.024-.026c-.267-.3-.622-.7-1.127-1.057a5.152 5.152 0 0 0-1.355-.678 3.001 3.001 0 0 0-5.896.04Z"
@@ -30,7 +30,7 @@ export default function Features() {
             className="inline-flex lg:w-[290px] w-[320px] items-center justify-between px-5 py-3 text-base font-medium text-center text-black border border-gray-200 bg-white rounded-xl hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mx-auto"
           >
             <div className="title flex gap-3 items-center font-bold">
-              <svg className="w-10 h-10 text-purple-800 rounded-xl  bg-purple-600 p-2 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <svg className="w-10 h-10 text-purple-500 rounded-xl  bg-purple-100 p-2 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M10 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16Z" />
                 <path fillRule="evenodd" d="M21.707 21.707a1 1 0 0 1-1.414 0l-3.5-3.5a1 1 0 0 1 1.414-1.414l3.5 3.5a1 1 0 0 1 0 1.414Z" clipRule="evenodd" />
               </svg>
@@ -45,7 +45,7 @@ export default function Features() {
             className="inline-flex lg:w-[290px] w-[320px] items-center justify-between px-5 py-3 text-base font-medium text-center text-black border border-gray-200 bg-white rounded-xl hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mx-auto"
           >
             <div className="title flex gap-3 items-center font-bold">
-              <svg className="w-10 h-10 text-green-800 rounded-xl bg-green-600 p-2 dark:text-whit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <svg className="w-10 h-10 text-green-500 rounded-xl bg-green-100 p-2 dark:text-whit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path
                   fillRule="evenodd"
                   d="M11 4.717c-2.286-.58-4.16-.756-7.045-.71A1.99 1.99 0 0 0 2 6v11c0 1.133.934 2.022 2.044 2.007 2.759-.038 4.5.16 6.956.791V4.717Zm2 15.081c2.456-.631 4.198-.829 6.956-.791A2.013 2.013 0 0 0 22 16.999V6a1.99 1.99 0 0 0-1.955-1.993c-2.885-.046-4.76.13-7.045.71v15.081Z"
@@ -63,7 +63,7 @@ export default function Features() {
             className="inline-flex lg:w-[290px] w-[320px] items-center justify-between px-5 py-3 text-base font-medium text-center text-black border border-gray-200 bg-white rounded-xl hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mx-auto"
           >
             <div className="title flex gap-3 items-center font-bold">
-              <svg className="w-10 h-10 text-red-800 rounded-xl bg-red-600 p-2 dark:text-whit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <svg className="w-10 h-10 text-red-500 rounded-xl bg-red-100 p-2 dark:text-whit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M13.5 2c-.178 0-.356.013-.492.022l-.074.005a1 1 0 0 0-.934.998V11a1 1 0 0 0 1 1h7.975a1 1 0 0 0 .998-.934l.005-.074A7.04 7.04 0 0 0 22 10.5 8.5 8.5 0 0 0 13.5 2Z" />
                 <path d="M11 6.025a1 1 0 0 0-1.065-.998 8.5 8.5 0 1 0 9.038 9.039A1 1 0 0 0 17.975 13H11V6.025Z" />
               </svg>
